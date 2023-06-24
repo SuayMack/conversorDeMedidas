bf3b8f3015dd4d70db5909606675816fcd495b77
SuayMack <pry_chu@hotmail.com>
2023-06-24 15:46:37 -0300

    criando as variáveis necessárias

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Seleciona o elemento HTML com o id 'categoria' e o armazena na variável 'categoria'
+var categoria = document.getElementById('categoria');
+
+// Seleciona o elemento HTML com o id 'entrada' e o armazena na variável 'entrada'
+var entrada = document.getElementById('entrada');
+
+// Seleciona o elemento HTML com o id 'unidade-entrada' e o armazena na variável 'unidadeEntrada'
+var unidadeEntrada = document.getElementById('unidade-entrada');
+
+// Seleciona o elemento HTML com o id 'unidade-saida' e o armazena na variável 'unidadeSaida'
+var unidadeSaida = document.getElementById('unidade-saida');
+
+// Seleciona o elemento HTML com o id 'unidades' e o armazena na variável 'selectUnidades'
+var selectUnidades = document.getElementById('unidades');
+
+// Cria uma nova opção para o elemento <select> com o texto "optionText" e o valor "optionValue"
+new Option("optionText", "optionValue");
